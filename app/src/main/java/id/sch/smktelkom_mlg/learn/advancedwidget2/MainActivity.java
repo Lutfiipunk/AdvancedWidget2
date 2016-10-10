659618f1cfa6ab8ec3736f87ec387de897a171e4
Lutfi <ipunkedan25@gmail.com>
2016-10-10 08:21:42 +0700

    initial commit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget2/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @BindColor(R.color.colorAccent)
     int colorAccent;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
